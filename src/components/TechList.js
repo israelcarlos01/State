7570e98d3ecfe386f35b56fe1759e41fc1f87fea
Israel Carlos <israelcarlos01@gmail.com>
2020-12-08 20:05:48 -0300

    Learning front-end

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
 
 class TechList extends Component {
-
+    //um estado do Ract é imutável, nada se adiciona. O array muda de estado!
     state = {
+        newTech: '',
         techs: [
             'node.js',
             'React.js',
             'React Native'
         ]
     };
+
+    handleInputChange = e => {
+        this.setState({ newTech: e.target.value })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        //O "..." copia tudo que já tem dentro do array e adiciona um novo dado no array
+        this.setState({ 
+            techs: [...this.state.techs, this.state.newTech],
+            newTech: '' 
+        });
+    }
+
     render() {
         return (
-            <>
+            <form onSubmit={this.handleSubmit}>
                 <ul>
                     {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}  
                 </ul>
-            </>
+                <input 
+                    type="text" 
+                    onChange= {this.handleInputChange} 
+                    value={this.state.newTech}
+                />
+                <button type="submit">Send</button>
+            </form>
         );
     }
 }
